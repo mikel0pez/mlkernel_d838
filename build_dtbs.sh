9d952a5a3aefde3327d7ac000774b5519e349d81
Mike Lopez <sergio.miguel.lopes@gmail.com>
2015-07-03 00:35:17 +0100

    Added DTBs Build Script

---
+++ build_dtbs.sh
@@ -0,0 +1,3 @@
+make ARCH=arm CROSS_COMPILE=~/android/arm-eabi-4.6/bin/arm-eabi- mike_defconfig msm8974-v2-b1-kr.dtb
+make ARCH=arm CROSS_COMPILE=~/android/arm-eabi-4.6/bin/arm-eabi- mike_defconfig msm8974-v2-2-b1-kr.dtb
+make ARCH=arm CROSS_COMPILE=~/android/arm-eabi-4.6/bin/arm-eabi- mike_defconfig msm8974-b1-kr.dtb
